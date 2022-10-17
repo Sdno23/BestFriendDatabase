a226b136fa99ef974a880c5d9af47be1f6986708
Sdno23 <103979026+Sdno23@users.noreply.github.com>
2022-10-16 17:59:29 -0700

    Create BestFriend.java

---
+++ BestFriend.java
@@ -0,0 +1,87 @@
+/*
+ * @author 3156908
+ *
+ * @version 1.0 09/06/2020
+ *
+ * Title: Brst Friend Array Tester
+ *
+ * Semester: Fall 2020
+ *
+ * Lecturer's Name: C. Charters
+ */
+
+package main;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author 3156908
+ */
+ 
+public class BestFriend {
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private String cellPhone;
+    private String email;
+
+    public BestFriend(String firstName, String lastName, String nickName, String cellPhone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.cellPhone = cellPhone;
+        this.email = email;
+    }
+    
+   
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "BestFriend{" + "firstName=" + firstName + ", lastName=" + lastName + ", nickName=" + nickName + ", cellPhone=" + cellPhone + ", email=" + email + '}';
+    }
+
+      
+       
+   
+    
+}
